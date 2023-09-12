f8bcc0a297a073ed17bc0113fdc020f224133628
mohamedelbadawi <38269461+mohamedelbadawi@users.noreply.github.com>
2023-09-12 17:41:06 +0300

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -2,11 +2,7 @@ const request = require('request-promise');
 const axios = require('axios')
 const fs = require('fs');
 exports.uploadToFacebook = async (pageId, accessToken, videoPath) => {
-    console.log(videoPath)
-    // const pageId = `104595038485033`
-    // const accessToken = 'EAAPPT067meoBOZC65TkrOMBOZAZALenznNPZBYgEwG9QB1ZCmAQkexy7Nv5Yn2L3Oi52ZAs4PhRUE7bcDPHRZAZBTbRQgqnr1Pqz06sMpxWoJwk0Rvw9kZCG8eDwO9ix4j38ZCkb4Mdxb04k48Q0VRb6659VZBnJy5gTDpnIXNOZB3hJ2ZC1TnFe44l22T1cZAZAcLDzpoZD'
-
-
+    
     const startUploadUrl = `https://graph.facebook.com/${pageId}/video_reels?upload_phase=start&access_token=${accessToken}`;
 
     const response = await axios.post(startUploadUrl);
@@ -20,7 +16,6 @@ exports.uploadToFacebook = async (pageId, accessToken, videoPath) => {
 
     const video = fs.readFileSync(videoPath)
 
-    console.log(video);
 
     const config = {
         headers: {
